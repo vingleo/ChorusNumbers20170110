cccdf85434360749bc26de3a41dacc377525cc30
vingleo <vingleo@vingleodemacbook-pro.local>
2017-01-22 23:36:38 +0800

    add comments

--- ChorusNumbers/ViewController.m
+++ ChorusNumbers/ViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by vingleo on 17/1/9.
 //  Copyright © 2017年 Vingleo. All rights reserved.
-//
+//  Use github 
 
 #import "ViewController.h"
 
@@ -19,9 +19,5 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
